5d993f15a124d5b3ab067a37aa454f15cdf2e29a
Lucas Ferronato <johndoe@example.com>
2020-04-03 23:09:37 -0300

    fix the profile start knex - dev to prod

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333);
-// console.log("backend rodando na porta: " + (process.env.PORT || 3333));
+// console.log("x");
+
+console.log("backend rodando na porta: " + (process.env.PORT || 3333));
 /** Interessante::
  * quando rodamos 'npm start' estamos chamando a config dentro do package.json/scripts:start
  * agora posso chamar 'npm dev' que executará via Nodemon
